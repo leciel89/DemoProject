3a29c69fa8ce2e5a53b2021c72b13ebd82996fb3
leciel89 <leciel89@gmail.com>
2017-03-14 23:22:24 +0700

    Add project files.

---
+++ Default/Control/Buttons.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Default.Control
+{
+   public class Buttons
+    {
+        #region variable
+
+        public string location;
+
+        #endregion
+
+        #region locator
+        static readonly By 
+
+        #endregion
+
+
+
+
+    }
+}
